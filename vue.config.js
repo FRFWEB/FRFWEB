1c8b0e3dfe6a6072fd5173a2842775cf9a5ca0e5
frfweb <francisco_glopez@hotmail.com>
2023-12-14 18:34:13 -0400

    update files

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  css: {
-    extract: {
-      filename: "assets/css/[name].css",
-    },
-  },
-  configureWebpack: (config) => {
-    config.output.filename = "assets/js/[name].js";
-    config.output.chunkFilename = "assets/js/[name].js";
-  },
 });
